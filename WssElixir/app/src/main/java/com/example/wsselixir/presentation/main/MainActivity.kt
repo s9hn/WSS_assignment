f576d30bb63083267ed9de2d9ba48e26576ed784
junseo511 <prime54@g.skku.edu>
2023-12-28 14:10:10 +0900

    feat : 팔로워 목록 생성

--- WssElixir/app/src/main/java/com/example/wsselixir/presentation/main/MainActivity.kt
+++ WssElixir/app/src/main/java/com/example/wsselixir/presentation/main/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.wsselixir.presentation.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.wsselixir.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.wsselixir.data.followers
+import com.example.wsselixir.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var followerAdapter: FollowerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        followerAdapter = FollowerAdapter()
+        binding.rvFollower.adapter = followerAdapter
+        binding.rvFollower.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        fetchFollowers()
+    }
+
+    private fun fetchFollowers() {
+        followerAdapter.submitList(followers)
     }
 }
