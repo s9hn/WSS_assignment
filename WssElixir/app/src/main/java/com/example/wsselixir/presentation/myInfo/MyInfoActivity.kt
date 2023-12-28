fa0c117d3aa64e598bad7a33d574cbebabe96b8e
librarywon <librarywon@naver.com>
2023-12-29 05:15:29 +0900

    feat : MBTI spinner & 화면 전환 구현

--- WssElixir/app/src/main/java/com/example/wsselixir/presentation/myInfo/MyInfoActivity.kt
+++ WssElixir/app/src/main/java/com/example/wsselixir/presentation/myInfo/MyInfoActivity.kt
@@ -1,12 +1,23 @@
 package com.example.wsselixir.presentation.myInfo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.wsselixir.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.wsselixir.databinding.ActivityMyInfoBinding
+import com.example.wsselixir.presentation.model.User
 
 class MyInfoActivity : AppCompatActivity() {
+    private lateinit var myInfoBinding: ActivityMyInfoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_info)
+        myInfoBinding = ActivityMyInfoBinding.inflate(layoutInflater)
+        setContentView(myInfoBinding.root)
+
+        initMyInfoUI()
+    }
+
+    private fun initMyInfoUI() {
+        val userInfo: User = intent.getParcelableExtra("userInfo") ?: return
+        myInfoBinding.tvMyInfoName.text = userInfo.name
+        myInfoBinding.tvMyInfoMBTI.text = userInfo.MBTI
     }
 }
