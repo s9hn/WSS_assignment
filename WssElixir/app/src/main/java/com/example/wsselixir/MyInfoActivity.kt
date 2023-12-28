c4f9294e975a830ba9705ce263379d0474732aae
librarywon <librarywon@naver.com>
2023-12-28 15:17:44 +0900

    feat : init HomeActivity, MyInfoActivity

---
+++ WssElixir/app/src/main/java/com/example/wsselixir/MyInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.wsselixir
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_info)
+    }
+}
