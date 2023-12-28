6ef937aecd1f9536182e48e92f2883fd908ed6da
m6z1 <sonmyungj1zz@gmail.com>
2023-12-29 00:05:06 +0900

    refactor: 패키지 분리

---
+++ WssElixir/app/src/main/java/com/example/wsselixir/presentation/home/HomeActivity.kt
@@ -0,0 +1,16 @@
+package com.example.wsselixir.presentation.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.wsselixir.R
+import com.example.wsselixir.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
