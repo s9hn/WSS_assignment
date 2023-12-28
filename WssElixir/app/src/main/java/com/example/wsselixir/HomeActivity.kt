b662963780a5bddd03c0861a0301779e43fcc066
librarywon <librarywon@naver.com>
2023-12-28 16:02:36 +0900

    feat : Followers 더미 데이터 추가

--- WssElixir/app/src/main/java/com/example/wsselixir/HomeActivity.kt
+++ WssElixir/app/src/main/java/com/example/wsselixir/HomeActivity.kt
@@ -8,4 +8,36 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
     }
+    companion object {
+        val dummyFollowers = listOf<Follower>(
+            Follower(
+                "https://avatars.githubusercontent.com/u/26512925?v=4",
+                "서재원"
+            ),
+            Follower(
+                "https://avat가ars.githubusercontent.com/u/26512925?v=4",
+                "김세훈"
+            ),
+            Follower(
+                "https://avatars.githubusercontent.com/u/26512925?v=4",
+                "손명지"
+            ),
+            Follower(
+                "https://avatars.githubusercontent.com/u/26512925?v=4",
+                "최준서"
+            ),
+            Follower(
+                "https://avatars.githubusercontent.com/u/26512925?v=4",
+                "이연진"
+            ),
+            Follower(
+                "https://avatars.githubusercontent.com/u/26512925?v=4",
+                "김명진"
+            ),
+            Follower(
+                "https://avatars.githubusercontent.com/u/26512925?v=4",
+                "백송현"
+            ),
+        )
+    }
 }
