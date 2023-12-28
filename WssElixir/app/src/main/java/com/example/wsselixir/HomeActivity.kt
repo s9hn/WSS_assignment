29132455edcfdde63c22ca14183b20b3dd7a55f9
librarywon <librarywon@naver.com>
2023-12-29 03:18:42 +0900

    feat : dialog 기능 구현

--- WssElixir/app/src/main/java/com/example/wsselixir/HomeActivity.kt
+++ WssElixir/app/src/main/java/com/example/wsselixir/HomeActivity.kt
@@ -1,11 +1,7 @@
 package com.example.wsselixir
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import com.example.wsselixir.remote.NetworkModule
-import kotlinx.coroutines.launch
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- WssElixir/app/src/main/java/com/example/wsselixir/presentation/home/Follower.kt
+++ WssElixir/app/src/main/java/com/example/wsselixir/presentation/home/Follower.kt
@@ -1,6 +1,7 @@
-package com.example.wsselixir
+package com.example.wsselixir.presentation.home
 
 data class Follower(
+    val id: Int,
     val profileImage: String,
     val name: String
 )
