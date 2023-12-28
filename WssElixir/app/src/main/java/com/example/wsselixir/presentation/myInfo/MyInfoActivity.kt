26442980341b207eb14206fa6fc090619d678648
librarywon <librarywon@naver.com>
2023-12-29 03:18:13 +0900

    chore : 폴더 구조 변경

--- WssElixir/app/src/main/java/com/example/wsselixir/presentation/myInfo/MyInfoActivity.kt
+++ WssElixir/app/src/main/java/com/example/wsselixir/presentation/myInfo/MyInfoActivity.kt
@@ -1,7 +1,8 @@
-package com.example.wsselixir
+package com.example.wsselixir.presentation.myInfo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.wsselixir.R
 
 class MyInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
