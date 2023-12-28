21bcb546b9ea374a4afa4725fab019c5716a5c41
m6z1 <sonmyungj1zz@gmail.com>
2023-12-28 22:17:25 +0900

    feat: 홈 뷰 구현

--- WssElixir/app/src/main/java/com/example/wsselixir/HomeActivity.kt
+++ WssElixir/app/src/main/java/com/example/wsselixir/HomeActivity.kt
@@ -3,9 +3,9 @@ package com.example.wsselixir
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
