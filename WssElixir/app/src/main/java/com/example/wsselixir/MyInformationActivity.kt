06f4a451a698f14b2b54129cb5f00c24464acc82
m6z1 <sonmyungj1zz@gmail.com>
2023-12-28 23:00:45 +0900

    feat: 내 정보 뷰 activity 추가

---
+++ WssElixir/app/src/main/java/com/example/wsselixir/MyInformationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.wsselixir
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MyInformationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_information)
+    }
+}
