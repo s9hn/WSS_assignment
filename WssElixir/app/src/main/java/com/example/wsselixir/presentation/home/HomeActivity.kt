1d3a4874e006338ef91be680836e66a61f73d387
m6z1 <sonmyungj1zz@gmail.com>
2023-12-29 15:28:54 +0900

    feat: 내 정보 뷰에 입력받은 값 띄우기

--- WssElixir/app/src/main/java/com/example/wsselixir/presentation/home/HomeActivity.kt
+++ WssElixir/app/src/main/java/com/example/wsselixir/presentation/home/HomeActivity.kt
@@ -2,6 +2,8 @@ package com.example.wsselixir.presentation.home
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -12,12 +14,16 @@ import com.example.wsselixir.presentation.myinformation.MyInformationActivity
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
 
+    private lateinit var userName: String
+    private lateinit var userMBTI: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         initSpinner()
+        selectSpinnerItemHandler()
         clickRegisterBtn()
     }
 
@@ -27,10 +33,31 @@ class HomeActivity : AppCompatActivity() {
         binding.spinnerHomeMBTI.adapter = adapter
     }
 
+    private fun selectSpinnerItemHandler() {
+        binding.spinnerHomeMBTI.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?, view: View?, position: Int, id: Long
+                ) {
+                    userMBTI = binding.spinnerHomeMBTI.getItemAtPosition(position).toString()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+            }
+    }
+
     private fun clickRegisterBtn() {
         binding.btnHomeRegister.setOnClickListener {
+            userName = binding.etHomeName.text.toString()
             if (validateMyInformation()) {
-                startActivity(Intent(this, MyInformationActivity::class.java))
+                val intent = Intent(this, MyInformationActivity::class.java)
+                intent.apply {
+                    putExtra("name", userName)
+                    putExtra("mbti", userMBTI)
+                }
+                startActivity(intent)
             } else {
                 Toast.makeText(this, ERROR_TOAST_MESSAGE, Toast.LENGTH_SHORT).show()
             }
@@ -38,10 +65,10 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun validateMyInformation(): Boolean {
-        return binding.etHomeName.text.toString().isNotBlank()
+        return userName.isNotBlank() && ::userMBTI.isInitialized
     }
 
     companion object {
-        const val ERROR_TOAST_MESSAGE = "이름을 입력하시오"
+        const val ERROR_TOAST_MESSAGE = "이름 및 MBTI를 입력하고 다시 시도하세요"
     }
 }
--- WssElixir/app/src/main/java/com/example/wsselixir/presentation/myinformation/MyInformationActivity.kt
+++ WssElixir/app/src/main/java/com/example/wsselixir/presentation/myinformation/MyInformationActivity.kt
@@ -2,11 +2,20 @@ package com.example.wsselixir.presentation.myinformation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.wsselixir.R
+import com.example.wsselixir.databinding.ActivityMyInformationBinding
 
 class MyInformationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMyInformationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_information)
+        binding = ActivityMyInformationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initMyInformation()
+    }
+
+    private fun initMyInformation() {
+        binding.tvMyInformationName.text = intent.getStringExtra("name")
+        binding.tvMyInformationMBTI.text = intent.getStringExtra("mbti")
     }
 }
