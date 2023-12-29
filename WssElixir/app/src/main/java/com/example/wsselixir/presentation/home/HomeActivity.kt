b37513e68471d218abc4506f831124ca2aedb9c9
m6z1 <sonmyungj1zz@gmail.com>
2023-12-29 15:04:55 +0900

    feat: 등록하기 버튼 클릭했을 때 로직 구현

--- WssElixir/app/src/main/java/com/example/wsselixir/presentation/home/HomeActivity.kt
+++ WssElixir/app/src/main/java/com/example/wsselixir/presentation/home/HomeActivity.kt
@@ -1,8 +1,11 @@
 package com.example.wsselixir.presentation.home
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.wsselixir.databinding.ActivityHomeBinding
+import com.example.wsselixir.presentation.myinformation.MyInformationActivity
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -11,5 +14,25 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        clickRegisterBtn()
+    }
+
+    private fun clickRegisterBtn() {
+        binding.btnHomeRegister.setOnClickListener {
+            if (validateMyInformation()) {
+                startActivity(Intent(this, MyInformationActivity::class.java))
+            } else {
+                Toast.makeText(this, ERROR_TOAST_MESSAGE, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun validateMyInformation(): Boolean {
+        return binding.etHomeName.text.toString().isNotBlank()
+    }
+
+    companion object {
+        const val ERROR_TOAST_MESSAGE = "이름을 입력하시오"
     }
 }
