7da93a08061afe1c66189e3b4373475f4d48face
m6z1 <sonmyungj1zz@gmail.com>
2023-12-29 17:25:52 +0900

    feat : 다이얼로그 화면 밖 터치 시 종료 막기

--- WssElixir/app/src/main/java/com/example/wsselixir/presentation/home/FollowerDialog.kt
+++ WssElixir/app/src/main/java/com/example/wsselixir/presentation/home/FollowerDialog.kt
@@ -21,7 +21,6 @@ class FollowerDialog : DialogFragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = DialogFollowerBinding.inflate(inflater, container, false)
-
         initDialog()
         clickCloseBtn()
         return binding.root
@@ -29,6 +28,7 @@ class FollowerDialog : DialogFragment() {
 
     private fun initDialog() {
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog?.setCancelable(false)
         val position = arguments?.getInt("position") ?: 0
         binding.ivHomeDialogPerson.setImageResource(followerMockList[position].img)
         binding.tvHomeDialogName.text = followerMockList[position].name
