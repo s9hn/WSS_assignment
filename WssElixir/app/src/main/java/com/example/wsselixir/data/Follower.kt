c249517bd44dd9efb90b86514cfc3bd56e41be8b
junseo511 <prime54@g.skku.edu>
2023-12-30 18:57:43 +0900

    refactor : followers를 private으로 수정

--- WssElixir/app/src/main/java/com/example/wsselixir/data/Follower.kt
+++ WssElixir/app/src/main/java/com/example/wsselixir/data/Follower.kt
@@ -5,12 +5,3 @@ data class Follower(
     val name: String,
     val profileImage: String
 )
-
-val followers = listOf(
-    Follower(1, "김명진", "https://avatars.githubusercontent.com/u/152427286?v=4"),
-    Follower(2, "김세훈", "https://avatars.githubusercontent.com/u/81347125?v=4"),
-    Follower(3, "백송현", "https://avatars.githubusercontent.com/u/153255948?v=4"),
-    Follower(4, "서재원", "https://avatars.githubusercontent.com/u/52442547?v=4"),
-    Follower(5, "손명지", "https://avatars.githubusercontent.com/u/114990782?v=4"),
-    Follower(6, "이연진", "https://avatars.githubusercontent.com/u/144861180?v=4"),
-)
