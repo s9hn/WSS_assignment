e2ba92bac036ef050728b8fbfd03400b7cbae6aa
librarywon <librarywon@naver.com>
2023-12-31 17:39:04 +0900

    refactor : Activity 간 결합도 줄이기 정적 메서드 사용

--- WssElixir/app/src/main/java/com/example/wsselixir/presentation/myInfo/MyInfoActivity.kt
+++ WssElixir/app/src/main/java/com/example/wsselixir/presentation/myInfo/MyInfoActivity.kt
@@ -1,5 +1,7 @@
 package com.example.wsselixir.presentation.myInfo
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.wsselixir.databinding.ActivityMyInfoBinding
@@ -20,5 +22,13 @@ class MyInfoActivity : AppCompatActivity() {
         val userInfo: User = intent.getParcelableExtra("userInfo") ?: return
         myInfoBinding.tvMyInfoName.text = userInfo.name
         myInfoBinding.tvMyInfoMBTI.text = userInfo.MBTI
+    }
+
+    companion object {
+        fun createIntent(context: Context, userInfo : User): Intent {
+            return Intent(context, MyInfoActivity::class.java).apply {
+                putExtra("userInfo", userInfo)
+            }
+        }
     }
 }
