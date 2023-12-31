25fbf4b0ebf365e1a3e65ca9222f3b1bbe9a37d0
librarywon <librarywon@naver.com>
2023-12-31 22:11:50 +0900

    refactor : ViewHolder분리, 다이얼로그 생성방식 변경

--- WssElixir/app/src/main/java/com/example/wsselixir/presentation/home/adapter/FollowerAdapter.kt
+++ WssElixir/app/src/main/java/com/example/wsselixir/presentation/home/adapter/FollowerAdapter.kt
@@ -4,13 +4,12 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.wsselixir.databinding.ItemFollowerBinding
 import com.example.wsselixir.presentation.model.Follower
 
 class FollowerAdapter(private val onItemClick: (Follower) -> Unit) :
-    ListAdapter<Follower, FollowerAdapter.FollowerViewHolder>(object : DiffUtil.ItemCallback<Follower>() {
+    ListAdapter<Follower, FollowerViewHolder>(object :
+        DiffUtil.ItemCallback<Follower>() {
         override fun areItemsTheSame(oldItem: Follower, newItem: Follower): Boolean {
             return oldItem.id == newItem.id
         }
@@ -21,27 +20,15 @@ class FollowerAdapter(private val onItemClick: (Follower) -> Unit) :
     }) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FollowerViewHolder {
-        val followerBinding =
-            ItemFollowerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return FollowerViewHolder(followerBinding)
+        val binding = ItemFollowerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return FollowerViewHolder(binding) { position ->
+            val item = getItem(position)
+            onItemClick(item)
+        }
     }
 
     override fun onBindViewHolder(holder: FollowerViewHolder, position: Int) {
-        holder.onBind(getItem(position))
-    }
-
-    inner class FollowerViewHolder(private val binding: ItemFollowerBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-
-        fun onBind(follower: Follower) {
-            binding.tvFollowerName.text = follower.name
-            Glide.with(itemView.context)
-                .load(follower.profileImage)
-                .into(binding.ivFollowerProfile)
-
-            binding.ivFollowerProfile.setOnClickListener {
-                onItemClick(follower)
-            }
-        }
+        val item = getItem(position)
+        holder.onBind(item)
     }
 }
