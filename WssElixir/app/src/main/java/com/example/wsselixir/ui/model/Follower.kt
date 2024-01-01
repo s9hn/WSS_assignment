aa14eb21044471c7a648e801525d79c80b7fdc8a
librarywon <librarywon@naver.com>
2024-01-02 02:07:57 +0900

    feat : User->LocalUser 네이밍 변경, Follower 모델 삭제

--- WssElixir/app/src/main/java/com/example/wsselixir/ui/model/Follower.kt
+++
@@ -1,7 +0,0 @@
-package com.example.wsselixir.ui.model
-
-data class Follower(
-    val id: Int,
-    val profileImage: String,
-    val name: String
-)
--- WssElixir/app/src/main/java/com/example/wsselixir/ui/model/LocalUser.kt
+++ WssElixir/app/src/main/java/com/example/wsselixir/ui/model/LocalUser.kt
@@ -4,7 +4,7 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class User(
+data class LocalUser(
     val name: String,
     val MBTI: String? = ""
 ) : Parcelable
