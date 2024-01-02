6c42644fada67152f3b33d56cc5575040b0bf4e0
m6z1 <sonmyungj1zz@gmail.com>
2024-01-02 11:57:39 +0900

    feat : TabLayout을 이용한 프래그먼트 설정

--- WssElixir/app/src/main/java/com/example/wsselixir/ui/detail/DetailActivity.kt
+++ WssElixir/app/src/main/java/com/example/wsselixir/ui/detail/DetailActivity.kt
@@ -2,7 +2,9 @@ package com.example.wsselixir.ui.detail
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.wsselixir.R
 import com.example.wsselixir.databinding.ActivityDetailBinding
+import com.google.android.material.tabs.TabLayout
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
@@ -11,13 +13,36 @@ class DetailActivity : AppCompatActivity() {
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // initMyInformation()
+        setupFragment()
+        selectTabListener()
     }
+
+    private fun setupFragment() {
+        supportFragmentManager.beginTransaction().replace(R.id.fcvDetail, MyInfoFragment()).commit()
+    }
+
+    private fun selectTabListener() {
+        binding.tabDetail.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                val fragment = when (tab.position) {
+                    0 -> MyInfoFragment()
+                    1 -> FollowerInfoFragment()
+                    else -> throw IllegalStateException("포지션이 없음")
+                }
+                supportFragmentManager.beginTransaction().replace(R.id.fcvDetail, fragment)
+                    .commit()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {
+            }
+        })
+    }
+}
 //    추후에 수정 예정
 //    private fun initMyInformation() {
-//        val name = intent.getStringExtra("name") ?: "0"
-//        val mbti = intent.getStringExtra("mbti") ?: "0"
 //        binding.tvMyInformationName.text = name
 //        binding.tvMyInformationMBTI.text = mbti
 //    }
-}
