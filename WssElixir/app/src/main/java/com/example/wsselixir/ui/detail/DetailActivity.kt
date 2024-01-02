6e9ed1c8454e6e1a670597bb7077db6f65184b16
librarywon <librarywon@naver.com>
2024-01-03 02:50:29 +0900

    feat : 팔로워 상세보기 뷰 구현 완료

--- WssElixir/app/src/main/java/com/example/wsselixir/ui/detail/DetailActivity.kt
+++ WssElixir/app/src/main/java/com/example/wsselixir/ui/detail/DetailActivity.kt
@@ -3,25 +3,96 @@ package com.example.wsselixir.ui.detail
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.example.wsselixir.R
 import com.example.wsselixir.databinding.ActivityDetailBinding
+import com.example.wsselixir.ui.detail.follower.FollowerFragment
+import com.example.wsselixir.ui.detail.user.UserFragment
 import com.example.wsselixir.ui.model.LocalUser
+import com.google.android.material.tabs.TabLayout
 
 class DetailActivity : AppCompatActivity() {
-    private lateinit var myInfoBinding: ActivityDetailBinding
+    private lateinit var detailBinding: ActivityDetailBinding
+    private val detailViewModel: DetailViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        myInfoBinding = ActivityDetailBinding.inflate(layoutInflater)
-        setContentView(myInfoBinding.root)
+        detailBinding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(detailBinding.root)
 
-        initMyInfoUI()
+        observeDetailUiState()
     }
 
-    private fun initMyInfoUI() {
+    private fun observeDetailUiState() {
+        detailViewModel.detailUiState.observe(this) {
+            when (it) {
+                is DetailUiState.Init -> {
+                    getIntentData()
+                    detailViewModel.updateFollowerInfo()
+                    setupInitFragment()
+                    setupTabLayout()
+                }
+                is DetailUiState.Success -> {
+                    makeToast("Follower Update에 성공하였습니다.")
+                }
+                is DetailUiState.Error -> {
+                    Log.e("updateFollower Error : ", it.message)
+                }
+            }
+        }
+    }
+
+    private fun getIntentData() {
         val localUserInfo: LocalUser = intent.getParcelableExtra("userInfo") ?: return
-        myInfoBinding.tvMyInfoName.text = localUserInfo.name
-        myInfoBinding.tvMyInfoMBTI.text = localUserInfo.MBTI
+        val followerId: Int = intent.getIntExtra("followerId",0)
+
+        detailViewModel.setLocalUserInfo(localUserInfo)
+        detailViewModel.setFollowerId(followerId)
+    }
+
+    private fun setupInitFragment() {
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add<FollowerFragment>(R.id.fcvDetail)
+        }
+        detailBinding.tbDetail.getTabAt(1)?.select()
+    }
+
+    private fun setupTabLayout() {
+        val tabLayout = detailBinding.tbDetail
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                when (tab.position) {
+                    0 -> showFragment(UserFragment::class.java)
+                    1 -> showFragment(FollowerFragment::class.java)
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {
+
+            }
+        })
+    }
+
+    private fun showFragment(fragmentClass: Class<out Fragment>) {
+        supportFragmentManager.commit {
+            replace(R.id.fcvDetail, fragmentClass, null)
+            setReorderingAllowed(true)
+        }
+    }
+
+    private fun makeToast(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
 
     companion object {
---
+++ WssElixir/app/src/main/java/com/example/wsselixir/ui/detail/DetailViewModel.kt
@@ -0,0 +1,45 @@
+package com.example.wsselixir.ui.detail
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.wsselixir.remote.NetworkModule
+import com.example.wsselixir.remote.UserResponseDto
+import com.example.wsselixir.ui.model.LocalUser
+import kotlinx.coroutines.launch
+
+class DetailViewModel : ViewModel() {
+    private var _followerId = MutableLiveData<Int>()
+    val followerId: LiveData<Int> get() = _followerId
+
+    private var _localUserInfo = MutableLiveData<LocalUser>()
+    val localUserInfo: LiveData<LocalUser> get() = _localUserInfo
+
+    private var _detailUiState = MutableLiveData<DetailUiState>(DetailUiState.Init)
+    val detailUiState: LiveData<DetailUiState> get() = _detailUiState
+
+    private var _follower = MutableLiveData<UserResponseDto.User>()
+    val follower: LiveData<UserResponseDto.User> get() = _follower
+
+    fun setFollowerId(followerId: Int) {
+        _followerId.value = followerId
+    }
+
+    fun setLocalUserInfo(localUserInfo: LocalUser) {
+        _localUserInfo.value = localUserInfo
+    }
+
+    fun updateFollowerInfo() {
+        viewModelScope.launch {
+            runCatching {
+                followerId.value?.let { NetworkModule.reqresApi.getUser(it) }
+            }.onSuccess {
+                _follower.value = it?.data
+                _detailUiState.value = DetailUiState.Success
+            }.onFailure {
+                _detailUiState.value = DetailUiState.Error(it.message ?: "error")
+            }
+        }
+    }
+}
