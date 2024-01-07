bf8743503ec310edff18ae363a0790045eebbfaf
librarywon <librarywon@naver.com>
2024-01-08 00:45:18 +0900

    feat: remote에 생긴 의존성 제거

--- WssElixir/app/src/main/java/com/example/wsselixir/ui/detail/DetailViewModel.kt
+++ WssElixir/app/src/main/java/com/example/wsselixir/ui/detail/DetailViewModel.kt
@@ -4,8 +4,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.wsselixir.data.mapper.toData
+import com.example.wsselixir.data.model.Follower
 import com.example.wsselixir.remote.NetworkModule
-import com.example.wsselixir.data.remote.response.UserResponseDto
 import com.example.wsselixir.ui.model.LocalUser
 import kotlinx.coroutines.launch
 
@@ -19,8 +20,8 @@ class DetailViewModel : ViewModel() {
     private var _detailUiState = MutableLiveData<DetailUiState>(DetailUiState.Init)
     val detailUiState: LiveData<DetailUiState> get() = _detailUiState
 
-    private var _follower = MutableLiveData<UserResponseDto.User>()
-    val follower: LiveData<UserResponseDto.User> get() = _follower
+    private var _follower = MutableLiveData<Follower>()
+    val follower: LiveData<Follower> get() = _follower
 
     fun setFollowerId(followerId: Int) {
         _followerId.value = followerId
@@ -36,7 +37,7 @@ class DetailViewModel : ViewModel() {
                 followerId.value?.let { NetworkModule.reqresApi.getUser(it) }
             }.onSuccess {
                 if (it?.data != null) {
-                    _follower.value = it.data
+                    _follower.value = it.data.toData()
                     _detailUiState.value = DetailUiState.Success
                 } else {
                     _detailUiState.value = DetailUiState.Error("Fetched data is null")
